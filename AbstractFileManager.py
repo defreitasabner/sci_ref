6aeb5836645353908aa3ce5abbc5e136ab007908
Abner Freitas <defreitasabner@gmail.com>
2022-02-19 21:49:46 -0300

    some updates

---
+++ AbstractFileManager.py
@@ -0,0 +1,24 @@
+from abc import ABCMeta, abstractmethod
+
+
+class AbstractFileManager(metaclass=ABCMeta):
+
+    @abstractmethod
+    def __create_file(self) -> None:
+        ...
+
+    @abstractmethod
+    def __read_file(self) -> None:
+        ...
+
+    @abstractmethod
+    def __write_file(self) -> None:
+        ...
+
+    @abstractmethod
+    def __update_file(self) -> None:
+        ...
+
+    @abstractmethod
+    def __export_data(self) -> str:
+        ...
---
+++ CsvFileManager.py
@@ -0,0 +1,32 @@
+import csv
+from typing import Dict
+
+from AbstractFileManager import AbstractFileManager
+
+
+class CsvFileManager(AbstractFileManager):
+
+    def __init__(self, input_path: str) -> None:
+        self.__input_path: str = input_path
+        self.__output_path: str = None
+        self.__reference_list: Dict[str, str] = None
+
+    def __create_file(self) -> None:
+        with open(file=self.__input_path, mode='x', encoding='utf-8-sig') as file:
+            pass
+
+    def __read_file(self) -> None:
+        with open(file=self.__input_path, mode='r', encoding='utf-8-sig') as file:
+            reference_list = csv.DictReader(file)
+            self.__reference_list = reference_list
+
+    def __write_file(self) -> None:
+        with open(file=self.__output_path, mode='w', encoding='utf-8-sig') as file:
+            pass
+
+    def __update_file(self) -> None:
+        with open(file=self.__input_path, mode='a', encoding='utf-8-sig') as file:
+            pass
+
+    def __export_data(self) -> Dict[str, str]:
+        return self.__data
